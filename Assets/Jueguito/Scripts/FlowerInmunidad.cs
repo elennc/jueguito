d863ac24cfa61a8c875527fbae1462dba96f0c64
mariigd <mariana.gomezd29@gmail.com>
2024-03-08 13:32:33 -0500

    aaaaaa

---
+++ Assets/Jueguito/Scripts/FlowerInmunidad.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+
+public class FlowerInmunidad : PickableItem
+{
+    protected override void Pick(GameObject picker)
+    {
+        MMEventManager.TriggerEvent(new PickableItemEvent(this));
+    }
+}
---
+++ Assets/Jueguito/Scripts/Inmunidad.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+
+public class Inmunidad : MonoBehaviour, MMEventListener<PickableItemEvent>
+{
+
+    [SerializeField]
+    private float tiempo_inmunidad;
+    private GameObject _fadeObject;
+    private Health salud_personaje;
+
+    [Header("Inmunidad")]
+    [MMInspectorButton("Control inmunidad")]
+    private bool ControlInmunidadaButton;
+
+   
+    void OnEnable()
+    {
+        this.MMEventStartListening<PickableItemEvent>();
+
+    }
+
+    void OnDisable()
+    {
+        this.MMEventStopListening<PickableItemEvent>();
+
+    }
+
+    public virtual void OnMMEvent(PickableItemEvent e)
+    {
+        if(e.PickableItem.name == "Flower")
+        {
+            ControlInmunidad();
+        }
+    }
+
+    public Health getHealth()
+    {
+        _fadeObject = LevelManager.Instance.Players[0].gameObject;
+        return _fadeObject.GetComponent<Health>();
+
+    }
+
+    public void ControlInmunidad()
+    {
+        salud_personaje = getHealth();
+        salud_personaje.Invulnerable = true;
+        StartCoroutine(DesactivarInmunidad());
+
+    }
+
+    public IEnumerator DesactivarInmunidad()
+    {
+        yield return new WaitForSeconds(tiempo_inmunidad);
+        salud_personaje.Invulnerable = false;
+    }
+}
