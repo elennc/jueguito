6c8bfceda184673398c239f22601d43b38969222
mariigd <mariana.gomezd29@gmail.com>
2024-03-20 00:08:33 -0500

    Nivel 2

---
+++ Assets/Jueguito/Scripts/ItemManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+
+public class ItemManager : MonoBehaviour, MMEventListener<PickableItemEvent>
+{
+    [SerializeField]
+    private GameObject puerta;
+    private int item_recolectado;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(puerta != null)
+        {
+            puerta.SetActive(false);
+        }
+    }
+
+    void OnEnable()
+    {
+        this.MMEventStartListening<PickableItemEvent>();
+    }
+
+    void OnDisable()
+    {
+        this.MMEventStopListening<PickableItemEvent>();
+
+    }
+
+    public virtual void OnMMEvent(PickableItemEvent e)
+    {
+        //Debug.Log(e.PickableItem.name);
+        //Debug.Log(e.PickableItem);
+        item_recolectado++;
+
+        if(item_recolectado == 1)
+        {
+            puerta.SetActive(true);
+        }
+    }
+}
